4b56f5c8cd78c68bcc2f460cb92b738419ce0977
Raymonldle <118484385+Raymonldle@users.noreply.github.com>
2025-04-02 17:48:45 +0000

    gayayt

--- CarTester.java
+++ CarTester.java
@@ -2,6 +2,22 @@ public class CarTester
 {
 	public static void run()
 	{
+
+		Car c = new Car("Toyota", 80);
+
+		System.out.println(c.getModel());
+		System.out.println(c.getFuelLevel());
+		System.out.println(c.milesLeft());
+
+		System.out.println(c.getModel(c));
+
+		ElectricCar ec = new ElectricCar("Toyota", 80);
+
+		System.out.println(ec.getModel());
+		System.out.println(ec.getFuelLevel());
+		System.out.println(ec.milesLeft(400));
+
+		System.out.println(ec);
 		// Create a Car object
 
 		// Print out the model
